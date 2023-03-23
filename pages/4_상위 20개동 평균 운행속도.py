5371f86bc5f95782914fdc44f5d0f08f137af24e
Yerial <a01063135213@gmail.com>
2023-03-24 03:01:52 +0900

    kor

--- pages/4_상위 20개동 평균 운행속도.py
+++ pages/4_상위 20개동 평균 운행속도.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import matplotlib
+import matplotlib as mpl
 import streamlit as st
 import pandas as pd
 import seaborn as sns
@@ -7,17 +8,13 @@ import matplotlib.pyplot as plt
 import platform
 
 # 한글 폰트 지정
-from matplotlib import font_manager, rc
-plt.rcParams['axes.unicode_minus'] = False
+import matplotlib.font_manager as fm
 
-if platform.system() == 'Darwin':  # 맥OS
-    rc('font', family='AppleGothic')
-elif platform.system() == 'Windows':  # 윈도우
-    path = "c:/Windows/Fonts/malgun.ttf"
-    font_name = font_manager.FontProperties(fname=path).get_name()
-    rc('font', family=font_name)
-else:
-    print('Unknown system...  sorry~~~')
+fe = fm.FontEntry(
+    fname='your custom ttf file path',
+    name='')
+fm.fontManager.ttflist.insert(0, fe) # or append is fine
+mpl.rcParams['font.family'] = fe.name # = 'your custom ttf font name'
 
 
 
